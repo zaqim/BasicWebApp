7cf3ccd132b26e43626feea6b010c2abd6b6b25d
Sefa Degirmenci <h.degirmenci@precitaste.com>
2022-07-13 15:51:53 +0200

    change the parse logic

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -7,7 +7,7 @@ public class QueryProcessor {
 
     public String process(String query) {
 		query = query.toLowerCase();
-        query.replace("%20","");
+        query.replace("%20"," ");
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
